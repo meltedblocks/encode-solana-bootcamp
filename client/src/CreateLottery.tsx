91da74966eb2aff2d144683fbbc320a18d38486b
f44z <vgood@protonmail.com>
2022-06-02 16:45:11 +0000

    Add anchor

--- client/src/CreateLottery.tsx
+++ client/src/CreateLottery.tsx
@@ -1,11 +1,15 @@
 import { WalletNotConnectedError } from '@solana/wallet-adapter-base';
 import { useConnection, useWallet } from '@solana/wallet-adapter-react';
 import { Keypair, SystemProgram, Transaction } from '@solana/web3.js';
+import * as anchor from "@project-serum/anchor";
+import { Program, Provider, web3, } from '@project-serum/anchor';
 import React, { FC, useCallback } from 'react';
+import idl from './idl.json';
 
 export const CreateLottery: FC = () => {
     const { connection } = useConnection();
     const { publicKey, sendTransaction } = useWallet();
+    const programID = new web3.PublicKey(idl.metadata.address);
 
     const onClick = useCallback(async () => {
         if (!publicKey) throw new WalletNotConnectedError();

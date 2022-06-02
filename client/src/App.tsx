b427734e2fac11edf1ca34c88002a8a806b5fb6c
f44z <vgood@protonmail.com>
2022-06-02 16:12:14 +0000

    Connect wallet

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { WalletAdapterNetwork, WalletError } from "@solana/wallet-adapter-base";
+import {
+  WalletDialogProvider,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-material-ui";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import {
+  GlowWalletAdapter,
+  PhantomWalletAdapter,
+  SlopeWalletAdapter,
+  SolflareWalletAdapter,
+  TorusWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+import { useSnackbar } from "notistack";
+import React, { FC, ReactNode, useCallback, useMemo } from "react";
+import { Theme } from "./Theme";
+import { CreateLottery } from "./CreateLottery";
+import { AppBar, Typography, Toolbar } from "@mui/material";
 
-function App() {
+export const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Theme>
+      <Context>
+        <Content />
+        <CreateLottery />
+      </Context>
+    </Theme>
   );
-}
+};
 
-export default App;
+const Context: FC<{ children: ReactNode }> = ({ children }) => {
+  // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
+  const network = WalletAdapterNetwork.Devnet;
+
+  // You can also provide a custom RPC endpoint.
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
+  // Only the wallets you configure here will be compiled into your application, and only the dependencies
+  // of wallets that your users connect to will be loaded.
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new GlowWalletAdapter(),
+      new SlopeWalletAdapter(),
+      new SolflareWalletAdapter({ network }),
+      new TorusWalletAdapter(),
+    ],
+    [network]
+  );
+
+  const { enqueueSnackbar } = useSnackbar();
+  const onError = useCallback(
+    (error: WalletError) => {
+      enqueueSnackbar(
+        error.message ? `${error.name}: ${error.message}` : error.name,
+        { variant: "error" }
+      );
+      console.error(error);
+    },
+    [enqueueSnackbar]
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} onError={onError} autoConnect>
+        <WalletDialogProvider>{children}</WalletDialogProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+const Content: FC = () => {
+  return (
+    <AppBar position="fixed">
+        <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Play for NFT
+          </Typography>
+      <WalletMultiButton />
+      </Toolbar>
+    </AppBar>
+  );
+};

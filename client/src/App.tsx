ef226cc7193267c1e80bfe9b18f5a82eb11bd69f
Kamal Ravichandran <itskamalhere@gmail.com>
2022-06-03 14:51:46 +0100

    Initialise lottery implemented

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import logo from "./logo.svg";
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { WalletAdapterNetwork, WalletNotConnectedError } from "@solana/wallet-adapter-base";
 import { WalletModalProvider, WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { ConnectionProvider, WalletProvider, useAnchorWallet } from "@solana/wallet-adapter-react";
 import {
@@ -8,7 +8,7 @@ import {
     SlopeWalletAdapter,
     SolflareWalletAdapter,
     TorusWalletAdapter,
-} from "@solana/wallet-adapter-wallets";  
+} from "@solana/wallet-adapter-wallets";
 import { clusterApiUrl, Connection, SystemProgram, SYSVAR_RENT_PUBKEY } from "@solana/web3.js";
 import { Program, AnchorProvider, web3, BN, utils } from "@project-serum/anchor";
 import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
@@ -17,6 +17,9 @@ import idl from "./idl.json";
 
 require("./App.css");
 require("@solana/wallet-adapter-react-ui/styles.css");
+
+//const network = "http://127.0.0.1:8899";
+const network = WalletAdapterNetwork.Devnet;
 
 const App: FC = () => {
     return (
@@ -28,8 +31,8 @@ const App: FC = () => {
 export default App;
 
 const Context: FC<{ children: ReactNode }> = ({ children }) => {
-    const network = WalletAdapterNetwork.Mainnet;
-    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+    const endpoint = useMemo(() => clusterApiUrl(network), []);
+    console.log(endpoint);
     const wallets = useMemo(
         () => [
           new PhantomWalletAdapter(),
@@ -38,7 +41,7 @@ const Context: FC<{ children: ReactNode }> = ({ children }) => {
           new SolflareWalletAdapter({ network }),
           new TorusWalletAdapter(),
         ],
-        [network]
+        []
       );
 
       return (
@@ -55,15 +58,14 @@ const Context: FC<{ children: ReactNode }> = ({ children }) => {
 
 const Content: FC = () => {    
     const wallet = useAnchorWallet();
-    const baseAccount = web3.Keypair.generate();
+    const endpoint = useMemo(() => clusterApiUrl(network), []);
+    const ownerAccount = wallet?.publicKey;
     
     function getProvider() {
         if(!wallet) {
-            return null;
+            throw new WalletNotConnectedError();
         }
-        const network = "http://127.0.0.1:8899";        
-        const connection = new Connection(network, "processed");
-        
+        const connection = new Connection(endpoint, "processed");
         const provider = new AnchorProvider(
             connection, wallet, {"preflightCommitment": "processed"}
         );
@@ -71,53 +73,54 @@ const Content: FC = () => {
     }
 
     async function initializeLottery() {
+        if (!ownerAccount) {
+            throw new WalletNotConnectedError();
+        }
         const provider = getProvider();
         const program = new Program(idl as any, idl.metadata.address, provider as any);
         const [lotteryOwner] = await web3.PublicKey.findProgramAddress(
-            [baseAccount.publicKey.toBytes(), utils.bytes.utf8.encode("lottery-owner-account")],
+            [ownerAccount?.toBytes(), utils.bytes.utf8.encode("lottery-owner-account")],
             program.programId
         );
-
         const [lottery] = await web3.PublicKey.findProgramAddress(
             [lotteryOwner.toBytes(), new BN(0).toArrayLike(Buffer, "le", 1), utils.bytes.utf8.encode("lottery")],
             program.programId
         );
-        const [lotteryTokenAccount, lotteryBump] = await web3.PublicKey.findProgramAddress(
+        const [lotteryTokenAccount] = await web3.PublicKey.findProgramAddress(
             [lottery.toBytes(), utils.bytes.utf8.encode("lottery-token-account")],
             program.programId
         );
-        
+        const ownerTokenAccount = "CtDptEdWFC9hNtAjtSVkQvqL5RWsUDmoatESxj4fYpmE";
+        const ownerTokenMint = "7tuNgpeoNiGLfKnwxbdNGXj5vvciPirYx9Z4kksT3GaV";        
         try {
-            await program.methods.initializeLottery(new BN(100), new BN(0),{
-                accounts: {
-                    owner: baseAccount.publicKey,
-                    lotteryOwner,
-                    //ownerTokenAccount,
-                    //ownerTokenMint,
-                    lottery,
-                    lotteryTokenAccount,
-                    rent: SYSVAR_RENT_PUBKEY,
-                    tokenProgram: TOKEN_PROGRAM_ID,
-                    systemProgram: SystemProgram.programId
-                }
-            })
+            await program.methods.initializeLottery(new BN(1), new BN(1))
+            .accounts({
+              owner: ownerAccount,
+              lotteryOwner,
+              ownerTokenAccount,
+              ownerTokenMint,
+              lottery,
+              lotteryTokenAccount,
+              rent: SYSVAR_RENT_PUBKEY,
+              tokenProgram: TOKEN_PROGRAM_ID,
+              systemProgram: SystemProgram.programId,
+            })            
+            .rpc();            
 
         } catch (err) {
-
+            console.log(err);
         }
     }
 
-
-
-    const walletAddress = wallet?.publicKey.toString();
     return (
         <>
             {(wallet &&
-                <p>Your wallet is {walletAddress}</p>) ||
+                <p>Your wallet is {ownerAccount?.toString()}</p>) ||
                 (<p>Click the button to connect</p>)
             }
             <WalletModalProvider>
                 <WalletMultiButton />
+                <button onClick={initializeLottery}> Initialize Lottery </button>
             </WalletModalProvider>
         </>
     );
